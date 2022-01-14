5e2d0d8864446a1543ed36b4661833c204f581f4
Torshken <pederhatlen@gmail.com>
2022-01-14 14:53:39 +0100

    Better illigal charracters removal (With php)

--- site/js/script.js
+++ site/js/script.js
@@ -1,18 +1,12 @@
 // Main script document, it will expand when i start work on more client side features
 // For every input tag
 document.querySelectorAll("input").forEach(element => {
-    // If a key other than 1, 0 or some other comfort buttons are pressed, prevent it from being registered (Does not currently work on mobile)
-    element.onkeydown = function(e) {
-        if(e.key != 0 && e.key != 1 && e.code != "Backspace" && e.code != "Tab" && e.code != "Enter" && !e.ctrlKey ) {
-            console.log(e.code);
-            e.preventDefault();
-        }
-    };
-    // Clear all charracters other than 1 or 0 from paste data (IE compatible)
-    element.onpaste = function(e) {
-        let paste = (e.clipboardData || window.clipboardData).getData('text/plain');
-        paste = paste.replace(/[^10]+/g, '')
-        element.value = paste;
-        e.preventDefault();
+    // If a key other than 1, 0 or some other comfort buttons are pressed, prevent it from being registered
+    element.oninput = function(e) {
+        element.value = cleanInput(element.value);
     };
 });
+
+function cleanInput(inn){
+    return inn.replace(/[^10 ]+/g, '');
+}
--- site/php/chatpost.php
+++ site/php/chatpost.php
@@ -6,11 +6,9 @@
 
     // Includes the same security as the chat page: Loginrequirement, Chat access and chatid, + post data for this page
     // Post requirement
-    if ($_SERVER["REQUEST_METHOD"] != "POST" or $_POST["usrmsg"] == null) {
+    if ($_SERVER["REQUEST_METHOD"] != "POST" or ctype_space($_POST["usrmsg"])) {
         header('Location: ../chat.php');
-    }else{
-        $msgtext = $_POST["usrmsg"];
-    }
+    }else{$msgtext = preg_replace("/[^10 ]+/", "", $_POST["usrmsg"]);}
 
     // Chat id is sett, and user is logged in
     if (isset($_SESSION["chatid"]) and isLoggedIn($con)) {
@@ -38,27 +36,3 @@
     //Redirect back to chat
     header('Location: chat.php');
 ?>
-
-<!DOCTYPE html>
-<html lang="no">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>BinærChat | Chat post</title>
-    <link rel="icon" type="image/png" href="../img/favicon.png">
-    <link rel="stylesheet" href="../css/style.css">
-</head>
-<body>
-    <header>
-        <h1><a href="../index.php">BinærChat</a></h1>
-    </header>
-    <div class="content">
-    </div>
-    <footer>
-        <span>Peder 2021</span>
-    </footer>
-    <script src="../js/script.js"></script>
-</body>
-</html>
