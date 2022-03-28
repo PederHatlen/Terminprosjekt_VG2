9eaafb3f4f462b73091eed5b368ecfa6cf25ae53
PederHatlen <pehaa002@osloskolen.no>
2022-03-28 12:29:16 +0200

    Userdeletion

--- SQL/Setup.sql
+++ SQL/Setup.sql
@@ -17,10 +17,11 @@ CREATE TABLE tokens (
     token VARCHAR(255) not null UNIQUE,
     created_at DATETIME not Null,
     expires_at DATETIME not Null,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    FOREIGN KEY (user_id) REFERENCES users(user_id) on delete cascade
 );
 
--- Is acctually just id, but needed a way to retrieve it after createon + created_at is usefull
+-- Is acctually just id and flags, but created by is good for testing/helping + created_at is usefull
+-- Created by is not foreign key, becouse i don't want to delete the conversation when one person has left.
 CREATE TABLE conversations (
     conversation_id INT not null PRIMARY KEY AUTO_INCREMENT,
     created_by INT not null,
@@ -34,8 +35,8 @@ CREATE TABLE conversation_users (
     user_id INT not null,
     color char(7) not null,
     isAdmin TINYINT NOT NULL DEFAULT 0,
-    FOREIGN KEY (conversation_id) REFERENCES conversations(conversation_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (conversation_id) REFERENCES conversations(conversation_id) on delete cascade,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) on delete cascade,
     PRIMARY KEY (conversation_id, user_id)
 );
 
@@ -46,8 +47,8 @@ CREATE TABLE messages (
     sender_id INT not null,
     messagetext VARCHAR(255) not null,
     sent_at DATETIME not Null DEFAULT current_timestamp,
-    FOREIGN KEY (conversation_id) REFERENCES conversations(conversation_id),
-    FOREIGN KEY (sender_id) REFERENCES users(user_id)
+    FOREIGN KEY (conversation_id) REFERENCES conversations(conversation_id) on delete cascade,
+    FOREIGN KEY (sender_id) REFERENCES users(user_id) on delete cascade
 );
 
 -- Basic help-ticket system
@@ -55,7 +56,6 @@ CREATE TABLE help_tickets (
     ticket_id INT not null PRIMARY KEY AUTO_INCREMENT,
     messagetext varchar(255) not null,
     email varchar(255) not null,
-    user_id int(255), -- Can be non existent if user f.x has problems logging in
-    sent_at DATETIME not Null DEFAULT current_timestamp,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    user_id int, -- Can be non existent if user fx. has problems logging in
+    sent_at DATETIME not Null DEFAULT current_timestamp
 );
--- site/php/brukerside.php
+++ site/php/brukerside.php
@@ -1,5 +1,6 @@
 <?php
     require 'phpRepo.php'; // PHPRepo is where all basic functions are: Connection, token, login etc.  
+    $msgText = '';
     
     $con = connect();
 
@@ -8,6 +9,56 @@
         header('Location: login.php');
         exit;
     }
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST" and isset($_POST["request"])) {
+        var_dump($_POST);
+        switch ($_POST["request"]) {
+            case 'remove':
+				// Preparing a statement for binding users to conversation, se Setup.sql for database structure
+                $stmt = $con->prepare('UPDATE conversation_users SET isAdmin = 1 WHERE conversation_id = ? and user_id = ?');
+                $stmt->bind_param('ii', $conversation_id, $insert_UID);
+
+				for ($i=0; $i < count($_POST)-1; $i++) { 
+					$tempVars = explode(",", $_POST[$i]);
+
+					$conversation_id = $tempVars[0];
+					$insert_UID = $tempVars[1];
+					$stmt->execute();
+				}
+
+                $stmt = $con->prepare('DELETE FROM users WHERE users.user_id = ?;');
+                $stmt->bind_param('i', $_SESSION["user_id"]);
+                $stmt->execute();
+
+                break;
+            case '':
+                break;
+        }
+    }
+
+    // $stmt = $con->prepare('SELECT * FROM conversation_users where user_id = ? and isAdmin = 1');
+	$stmt = $con->prepare('SELECT conversation_users.conversation_id, users.user_id,users.username FROM conversation_users 
+	join conversation_users otherUsers on conversation_users.conversation_id = otherUsers.conversation_id and not conversation_users.user_id = otherUsers.user_id
+	join users on otherUsers.user_id = users.user_id
+	WHERE conversation_users.user_id = ? and conversation_users.isAdmin = 1;');
+	$stmt->bind_param('i', $_SESSION["user_id"]);
+	$stmt->execute();
+	$result = $stmt->get_result()->fetch_all();
+
+	$index = [];
+	$conversations = [];
+	$j = 0;
+	for ($i=0; $i < count($result); $i++) {
+		if (!isset($index[$result[$i][0]])){
+			$index[$result[$i][0]] = $j;
+			$conversations[$j][0] = $result[$i][0];
+			$conversations[$j][1][] = $result[$i][1];
+			$conversations[$j][2][] = ($result[$i][2] == "1"? "Torshken":$result[$i][2]);
+			$j++;
+		}else{
+			$conversations[$index[$result[$i][0]]][2][] = ($result[$i][2] == "1"? "Torshken":$result[$i][2]);
+		}
+	}
 
     $con->close();
 ?>
@@ -24,20 +75,41 @@
     <!-- Main nav-bar, or header -->
     <?php include 'header.php';?>
     <main>
-        <h2>Brukerside for <?php echo $_SESSION["username"];?></h2>
+        <h2>Instillinger for <?php echo $_SESSION["username"];?></h2>
         <div id="settings">
-            <h3>Instillinger</h3>
-            <form action="" method="post">
-                <button onclick="">Slett bruker</button>
-            </form>
-        </div class="fullPopup">
+            <button onclick="removeAccountEl.style.display = 'flex'" class="deleteBtn">Slett bruker</button>
+        </div>
+        <div class="fullPopup" id="removeAccount">
             <div>
                 <h2>Er du sikker på at du vil slette brukeren din?</h2>
+                <p>Dette er ikke reversibelt, og vil fjerne <u><b>ALT</b></u> du har skrevet og gjort i binærchat.</p>
+                <form action="" method="post" id="delUsrForm">
+						<?php
+							if (count($conversations)!= 0) {
+								echo "<hr><p>Før du sletter brukeren din må du sette en administrator i chattene dine.</p>
+								<table id=\"useradmintable\"><tr id='tableHeader'><th>Personer&nbsp;</th><th>Velg admin</th></tr>";
+								
+								for ($i=0; $i < count($conversations); $i++) {
+									echo "<tr><td><label for='convAdminSelect'>[".implode(', ', $conversations[$i][2])."]</label></td>
+									<td><select name='convAdminSelect".$conversations[$i][0]."' form='delUsrForm'>";
+									for ($j=0; $j < count($conversations[$i][1]); $j++) {echo "<option value='".$conversations[$i][0].",".$conversations[$i][1][$j]."'>".$conversations[$i][2][$j]."</option>";}
+									echo "</select></td></tr>";
+								}
+								echo "</table><br>";
+							}
+						?>
+                    <input type="hidden" name="request" value="remove">
+                    <input type="submit" class="deleteBtn" value="Jeg vil slette brukeren min">
+                    <button onclick="removeAccountEl.style.display = ''">Nei, ta meg tilbake</button>
+                </form>
             </div>
         <div>
     </main>
     <!-- Footer for extra info -->
     <script src="../js/script.js"></script>
     <?php include 'footer.php';?>
+    <script>
+        let removeAccountEl = document.getElementById("removeAccount");
+    </script>
 </body>
 </html>
