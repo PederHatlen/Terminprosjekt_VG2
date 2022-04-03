e2a7e697ea0f9db66d92fddc1457f62be18e79ac
Peder Hatlen <pehaa002@osloskolen.no>
2022-04-03 13:22:50 +0200

    logoff is more violent

--- site/php/logoff.php
+++ site/php/logoff.php
@@ -1,10 +1,11 @@
 <?php
-    // Small document for logging user off. PHP is before the document because redirecting does not work otherwise
-
-    require 'phpRepo.php';
+    // Small document for logging user off.
+    session_start();
     // like if user is in vallid, session variables are cleared
     unset($_SESSION["username"]);
     unset($_SESSION["logintoken"]);
     unset($_SESSION["user_id"]);
+    
+    session_destroy();
     header('Location: ../index.php');
 ?>
