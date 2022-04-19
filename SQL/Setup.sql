1ad16f24b17d095f9f94645a47f4c2fc70a4ac22
Peder Hatlen <pehaa002@osloskolen.no>
2022-04-19 20:00:33 +0200

    fixed ordering

--- SQL/Setup.sql
+++ SQL/Setup.sql
@@ -20,19 +20,6 @@ CREATE TABLE tokens (
 	FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
--- Websocket tokens, used instead of having user data in client
-CREATE TABLE ws_tokens (
-	ws_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	conversation_id INT NOT NULL,
-	user_id INT NOT NULL,
-	token VARCHAR(255) NOT NULL,
-	active TINYINT NOT NULL DEFAULT 1,
-	expires_at DATETIME NOT NULL DEFAULT DATE_ADD(NOW(), INTERVAL 15 SECOND),
-	FOREIGN KEY (conversation_id) REFERENCES conversations(conversation_id) ON DELETE CASCADE,
-	FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
-	UNIQUE(token, active)
-);
-
 -- Is acctually just id and flags, but created by is good for testing/helping + created_at is usefull
 -- Created by is not foreign key, becouse i don't want to delete the conversation when one person has left.
 CREATE TABLE conversations (
@@ -51,6 +38,19 @@ CREATE TABLE conv_users (
 	FOREIGN KEY (conversation_id) REFERENCES conversations(conversation_id) ON DELETE CASCADE,
 	FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
 	PRIMARY KEY (conversation_id, user_id)
+);
+
+-- Websocket tokens, used instead of having user data in client
+CREATE TABLE ws_tokens (
+	ws_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	conversation_id INT NOT NULL,
+	user_id INT NOT NULL,
+	token VARCHAR(255) NOT NULL,
+	active TINYINT NOT NULL DEFAULT 1,
+	expires_at DATETIME NOT NULL DEFAULT DATE_ADD(NOW(), INTERVAL 15 SECOND),
+	FOREIGN KEY (conversation_id) REFERENCES conversations(conversation_id) ON DELETE CASCADE,
+	FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+	UNIQUE(token, active)
 );
 
 -- All the messages from all the chats are stored here, thay are retrieved by conversation_id
