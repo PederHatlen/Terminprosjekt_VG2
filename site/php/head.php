c280805153c7a9d57cfd7b9b59391a1abc97c231
Peder <pederhatlen@gmail.com>
2022-04-29 12:22:13 +0200

    is unicorn mode allowed & activated

--- site/php/head.php
+++ site/php/head.php
@@ -15,7 +15,9 @@
 <link rel="icon" type="image/png" href="/BinærChat/img/favicon.png">
 <link rel="stylesheet" href="/BinærChat/css/style.css">
 <?php
+	$unicorn = false;
 	if (isset($_SESSION["unicorn"]) and constant("allowUnicornMode") == true){
+		$unicorn = true;
 		echo "<link rel=\"stylesheet\" href=\"/BinærChat/css/themes/unicorn.css\">";
 	}else{
 		echo "<link rel=\"stylesheet\" href=\"/BinærChat/css/themes/normal.css\">";
