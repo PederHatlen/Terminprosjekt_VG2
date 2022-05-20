d417da48d3e1bc8c80f788e8f5a39af67a531e64
Peder <pederhatlen@gmail.com>
2022-05-20 11:20:57 +0200

    fixed themes

--- site/php/chat.php
+++ site/php/chat.php
@@ -217,7 +217,7 @@
 					
 					$class .= "\"";
 					$msgColor = "";
-					$msgColor = (isset($_SESSION["theme"])? "":"style=\"color: ". $messages[$i][1] .";\"");
+					$msgColor = (!isset($_SESSION["theme"])? "style=\"color: ". $messages[$i][1] .";\"":"");
 
 					echo("<p><span $class $msgColor><span class='time'>[" . $fdate . "]</span> " . ($messages[$i][0]==1? "Torshken":$messages[$i][0]) . ":</span> " . $messages[$i][2] . "</p>");
 				}
@@ -237,7 +237,7 @@
 	<!-- Script is integrated because of data from php that needs to be integrated. -->
 	<script>
 		const initData = {wsToken:"<?php echo $wsToken;?>"};
-		const theme = <?php echo isset($_SESSION["theme"]);?>;
+		const theme = <?php echo (isset($_SESSION["theme"])? "true":"false");?>;
 	</script>
 	<script src="../js/chatScript.js"></script>
 	<script src="../js/script.js"></script>
